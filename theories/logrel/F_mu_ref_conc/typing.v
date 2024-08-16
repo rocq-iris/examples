6b124185832b13e73a06ee6c28a37a096ffabe04
Robbert Krebbers <mail@robbertkrebbers.nl>
2024-08-16 09:39:33 +0200

    Forwards compatibility fix for https://gitlab.mpi-sws.org/iris/stdpp/-/merge_requests/555

--- theories/logrel/F_mu_ref_conc/typing.v
+++ theories/logrel/F_mu_ref_conc/typing.v
@@ -113,8 +113,8 @@ Proof.
   - rename select (_ !! _ = Some _) into H.
     apply lookup_lt_Some in H. rewrite iter_up. destruct lt_dec; auto with lia.
   - f_equal. eauto.
-  - f_equal. rewrite ->map_length in *. done.
-  - rewrite ->map_length in *; by f_equal.
+  - f_equal. rewrite ->fmap_length in *. done.
+  - rewrite ->fmap_length in *; by f_equal.
 Qed.
 
 (** Weakening *)
